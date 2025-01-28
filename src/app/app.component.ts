c240b1ca3a8e2c46651c5cff02bb24bbbdf6b2ec
Michaël Binacchi <michael.binacchi@worldline.com>
2025-01-28 14:38:12 +0100

    TP : Décomposer l'application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core'
-import { FormsModule } from '@angular/forms'
-import { LoginFormComponent } from './components/login-form/login-form.component'
-import { RouterOutlet } from '@angular/router'
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LoginFormComponent } from '@components/login-form/login-form.component';
+import { RouterOutlet } from '@angular/router' ;
+import { FilmSearchComponent } from '@components/film-search/film-search.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,LoginFormComponent],
+  imports: [RouterOutlet,LoginFormComponent,FilmSearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-title = 'Search Films'
+  title = 'Search Films';
+  loggedIn=false;
+  onLogin(loggedIn :boolean) {
+  this.loggedIn=loggedIn;
+  }
+
 }
